ccc816996e80680f2759ad2ad2c1a3dc723fb167
huniiali1122@gmail.com <huniiali1122@gmail.com>
2026-01-19 23:45:46 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-gull bg-black text-white flex items-center justify-center'>
+      <h1>hey</h1>
+    </div>
+  )
+}
+
+export default App
