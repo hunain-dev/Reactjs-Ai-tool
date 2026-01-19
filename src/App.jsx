89938420eae673afeff3c3c6f2c16d0c3732fae5
huniiali1122@gmail.com <huniiali1122@gmail.com>
2026-01-20 03:27:17 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Sidebar from './Common/Sidebar';
+import { FaArrowTrendUp } from "react-icons/fa6";
 
 const App = () => {
-  return (
-    <div className='h-screen w-gull bg-black text-white flex items-center justify-center'>
-      <h1>hey</h1>
-    </div>
-  )
-}
+  const [Askquestion, setAskquestion] = useState("")
 
-export default App
+
+  const Questionhandler = (e) =>{
+    e.preventDefault();
+    setAskquestion('')
+    console.log("your Quesiton is", Askquestion)
+  }
+
+
+  return (
+    <div className="h-screen w-full flex bg-gradient-to-l from-amber-50 to-white">
+      
+      {/* Left Sidebar */}
+      <Sidebar />
+
+      {/* Right main content */}
+      <div className=" Actay flex items-center justify-between flex-col h-screen  p-4 w-full">
+        <div className='h-[10vh] flex items-center justify-center w-full'>
+          <h2 className=' text-center Actay text-[1.6vw] '>Hellow Users Ask me Everything</h2>
+        </div>
+        <div className=' w-full'>
+        <div className=" flex items-center justify-center  px-25 p-4   rounded-3xl ">
+      <form  className='h-full w-full' onSubmit={Questionhandler}>
+      <input
+      value={Askquestion}
+      onChange={(e)=>{
+        setAskquestion(e.target.value)
+
+      }}
+        type="text"
+        placeholder="Type your message..."
+        className="flex-1  text-black rounded-full px-4 py-3 focus:outline-none focus:ring-1 focus:ring-black placeholder-gray border-1 border-gray-500 "
+      />
+
+      {/* Send button */}
+      <button  className="ml-2 bg-black  text-white p-3 rounded-full ">
+      <FaArrowTrendUp className='text-2xl' />
+
+      </button>
+      </form>
+
+    </div>
+        </div>
+       
+      
+      </div>
+
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Common/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Sidebar = () => {
+  return (
+    <div className="h-screen w-80 bg-gray-100 shadow-lg shadow-amber-100 rounded-3xl p-4">
+      {/* Sidebar content */}
+ 
+    </div>
+  );
+};
+
+export default Sidebar;
